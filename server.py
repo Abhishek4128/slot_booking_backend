f3bfaf4fd6fcad9ec7ab735840a9563f0c9da149
Abhishek4128 <abhishekmandal0204@gmail.com>
2026-03-06 01:44:36 +0530

    Update server.py

--- server.py
+++ server.py
@@ -27,7 +27,7 @@ DB_CONFIG = {
 SERVER_PORT   = 8000
 # BASE_DIR     = Path(__file__).resolve().parent.parent
 # FRONTEND_DIR = BASE_DIR / 'frontend'
-ALLOWED_ORIGIN = 'https://glowing-pika-f770ac.netlify.app/'   
+ALLOWED_ORIGIN = 'https://glowing-pika-f770ac.netlify.app'   
 
 
 
